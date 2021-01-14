513078103fcca1ebe334dfe48d8e113f42a2fb58
Joseph <joseph@stechstudio.com>
2021-01-14 16:03:27 -0500

    FilePond handler

---
+++ src/Handler/FilePondUploadHandler.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace Pion\Laravel\ChunkUpload\Handler;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\UploadedFile;
+use Pion\Laravel\ChunkUpload\Config\AbstractConfig;
+use Pion\Laravel\ChunkUpload\Save\AbstractSave;
+use Pion\Laravel\ChunkUpload\Save\ChunkSave;
+use Pion\Laravel\ChunkUpload\Storage\ChunkStorage;
+
+class FilePondUploadHandler extends AbstractHandler
+{
+    const CHUNK_UUID_INDEX = 'patch';
+
+    const HEADER_UPLOAD_LENGTH = 'Upload-Length';
+    const HEADER_UPLOAD_OFFSET = 'Upload-Offset';
+    const HEADER_UPLOAD_NAME = 'Upload-Name';
+
+    /**
+     * Checks if the current abstract handler can be used via HandlerFactory.
+     *
+     * @param Request $request
+     *
+     * @return bool
+     */
+    public static function canBeUsedForRequest(Request $request)
+    {
+        return $request->hasHeader(self::HEADER_UPLOAD_OFFSET);
+    }
+
+    /**
+     * FilePond uploads chunks by just putting the data right in the PATCH body.
+     * There is no UploadedFile created on the request. We have to save the payload
+     * to a temporary file and create it ourself.
+     *
+     * @param $fileIndex
+     * @param Request $request
+     *
+     * @return UploadedFile|null
+     */
+    public static function getUploadedFile($fileIndex, Request $request)
+    {
+        $path = tempnam(sys_get_temp_dir(), $fileIndex . "_");
+        file_put_contents($path, $request->getContent());
+
+        return new UploadedFile(
+            $path,
+            $request->header(self::HEADER_UPLOAD_NAME),
+            null,
+            \UPLOAD_ERR_OK,
+            true
+        );
+    }
+
+    /**
+     * @return int
+     */
+    public function getTotalSize()
+    {
+        return $this->request->header(self::HEADER_UPLOAD_LENGTH);
+    }
+
+    /**
+     * @return int
+     */
+    public function getChunkOffset()
+    {
+        return $this->request->header(self::HEADER_UPLOAD_OFFSET);
+    }
+
+    /**
+     * @return int
+     */
+    public function getChunkSize()
+    {
+        return $this->file->getSize();
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileName()
+    {
+        return $this->request->header(self::HEADER_UPLOAD_NAME);
+    }
+
+    /**
+     * @return string
+     */
+    public function getFileUUID()
+    {
+        return $this->request->get(self::CHUNK_UUID_INDEX);
+    }
+
+    /**
+     * Creates save instance and starts saving the uploaded file.
+     *
+     * @param ChunkStorage $chunkStorage the chunk storage
+     *
+     * @return AbstractSave
+     */
+    public function startSaving($chunkStorage)
+    {
+        return new ChunkSave($this->file, $this, $chunkStorage, $this->config);
+    }
+
+    /**
+     * Returns the chunk file name for a storing the tmp file.
+     *
+     * @return string
+     */
+    public function getChunkFileName()
+    {
+        return $this->createChunkFileName($this->getFileUUID(), $this->getChunkOffset());
+    }
+
+    /**
+     * Checks if the request has first chunk.
+     *
+     * @return bool
+     */
+    public function isFirstChunk()
+    {
+        return $this->getChunkOffset() == 0;
+    }
+
+    /**
+     * Checks if the current request has the last chunk.
+     *
+     * @return bool
+     */
+    public function isLastChunk()
+    {
+        return $this->getChunkOffset() + $this->getChunkSize() == $this->getTotalSize();
+    }
+
+    /**
+     * Checks if the current request is chunked upload.
+     *
+     * @return bool
+     */
+    public function isChunkedUpload()
+    {
+        return true;
+    }
+
+    /**
+     * Returns the percentage of the upload file.
+     *
+     * @return int
+     */
+    public function getPercentageDone()
+    {
+        return ($this->getChunkOffset() + $this->getChunkSize()) / $this->getTotalSize() * 100;
+    }
+}
